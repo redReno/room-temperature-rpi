bc4b00a8b6618df0684179bffb09c0005d7a4f46
Alf Stenbrunn <5901432+redReno@users.noreply.github.com>
2020-04-12 14:42:53 +0100

    Make led control run as own process
    Spawns a subprocess of the c program that only controls the led.
    Sends a SIGINT signal to the process before sending the request
    to get a temperature reading. The signal handler will turn the led
    on and off. Kill <PID> or ctrl+c will terminate both the led control
    and the server.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const util = require('util');
 const bodyParser = require('body-parser');
 const exec = util.promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
 const port = process.env.port || 8082
 const rpath = {root: __dirname + '/views'};
 
@@ -14,9 +15,14 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', rpath);
 })
 
+// start c program running in bg
+const led_controller = spawn(__dirname + '/src/rpi_temp.out', ['0', '1']);
+
 app.get('/temperature', async function (req, res) {
-  var temperature = null;
-  const { error, stdout, stderr } = await exec(__dirname + '/src/rpi_temp.out', { shell: true });
+  var temperature = null
+  // send signal to turn on led
+  led_controller.kill('SIGINT')
+  const { error, stdout, stderr } = await exec(__dirname + '/src/rpi_temp.out', { debug: 1 });
   if (typeof error !== 'undefined') {
     console.error(stderr);
     res.send(stderr);
@@ -32,6 +38,22 @@ app.use('*',function(req, res){
   res.send('Error 404: Not Found!');
 });
 
-app.listen(port, function () {
-  console.log(`Temperature app listening on ${port}!`);
+var server = app.listen(port, function () {
+  console.log(`Temperature app listening on ${port}! PID: ${process.pid}`);
+})
+
+function stopServer() {
+  console.log('\nSIGINT. Closing server...');
+  server.close();
+  led_controller.kill('SIGTERM')
+  process.exit(0);
+}
+
+// callbacks
+process.on('SIGINT', () => {
+  stopServer()
+})
+
+process.on('SIGTERM', () => {
+  stopServer()
 })
